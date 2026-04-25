416271786afe9142fb6fdd4a0760c4c3402324cf
purok-kaligtasan133 <leymedallada@gmail.com>
2026-04-25 22:15:43 +0800

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// config.js - Central configuration for all pages
-const API_URL = "https://script.google.com/macros/s/AKfycbwGFL1xNeJ4E78CVpnaQKMdx_RFYDm0jVMbVJ-KnWTE2NbNrhin6bIgr5_R8sEgC4ZGeA/exec";
-
-// App version (optional, for cache busting)
-const APP_VERSION = "2.0";
-
-// Other global settings
-const MAX_FILE_SIZE_MB = 10;
-const SUPPORTED_FILE_TYPES = ['image/jpeg', 'image/png', 'image/gif', 'image/bmp', 'image/tiff', 'application/pdf'];
